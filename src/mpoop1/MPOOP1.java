4e4e13c097f4bec245adbf3aec15a435f6cfc660
USER <USER@EMILIANO-SC>
2021-06-22 20:45:11 -0500

    update

---
+++ src/mpoop1/MPOOP1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpoop1;
+
+/**
+ *
+ * @author EmilianoSanchez
+ * @version 1.2 15.6.2021
+ */
+public class MPOOP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("¡Hola mundo!");
+    }
+    
+}
---
+++ src/mpoop1/NuevoMain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpoop1;
+
+/**
+ *
+ * @author USER
+ */
+public class NuevoMain {
+    public static void main(String[] args){
+        System.out.println("Este es el hola mundo 2");
+        
+    }
+    
+    
+    
+}
